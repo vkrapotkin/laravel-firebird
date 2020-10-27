71086cc02802821f7fe6333694a293f5a9e3d4cc
Harry Gulliford <harry@harrygulliford.com>
2020-10-27 12:39:11 +1100

    Fix static on const

--- src/Support/Version.php
+++ src/Support/Version.php
@@ -4,10 +4,10 @@ namespace Firebird\Support;
 
 class Version
 {
-    public static const FIREBIRD_15 = '1.5';
-    public static const FIREBIRD_25 = '2.5';
+    public const FIREBIRD_15 = '1.5';
+    public const FIREBIRD_25 = '2.5';
 
-    public static const SUPPORTED_VERSIONS = [
+    public const SUPPORTED_VERSIONS = [
         self::FIREBIRD_15,
         self::FIREBIRD_25,
     ];
