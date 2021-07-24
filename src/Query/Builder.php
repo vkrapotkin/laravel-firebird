d62b75a7d4a7163d29214a9a5942b2c804e7f289
Harry Gulliford <harry@harrygulliford.com>
2021-07-24 14:20:36 +1000

    Add count to query builder

--- src/Query/Builder.php
+++ src/Query/Builder.php
@@ -7,6 +7,16 @@ use Illuminate\Database\Query\Builder as QueryBuilder;
 class Builder extends QueryBuilder
 {
     /**
+     * Determine if any rows exist for the current query.
+     *
+     * @return bool
+     */
+    public function exists()
+    {
+        return parent::count() > 0;
+    }
+
+    /**
      * Add a from stored procedure clause to the query builder.
      *
      * @param string $procedure
